baa9a84fc69c370a79cbc6a29a4efd8511451a8c
Artyom Vasich <vasich.dev@gmail.com>
2025-04-15 23:19:23 +0400

    cli update

--- git_cli/src/main.rs
+++ git_cli/src/main.rs
@@ -1,9 +1,10 @@
+use std::error::Error;
+
 use clap::{Args, Parser, Subcommand};
 use git_core::{AuthType, Config, Repo, git::branch_name};
-use itertools::Itertools;
 
 
-fn main() -> Result<(), git2::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     let repo_name = std::env::var("REPO_NAME").expect("repo name is not set");
     let root_path = std::env::var("REPO_ROOT_PATH").expect("repo root path is not set");
 
@@ -14,42 +15,50 @@ fn main() -> Result<(), git2::Error> {
         path: format!("{root_path}/test_repo/{repo_name}").into(),
     };
 
-    let repo = Repo::open(&config).expect("failed to open repository");
-
     match Cli::parse().command {
+        Commands::Clone(CloneArgs { url }) => {
+            let _repo = Repo::clone_from(&url, &config)?;
+            config
+                .path
+                .read_dir()?
+                .flatten()
+                .for_each(|file| println!("{}", file.file_name().to_string_lossy()));
+        },
         Commands::Add(AddArgs { files: _files }) => {
+            let repo = Repo::open(&config).expect("failed to open repository");
             let index = repo.add_all()?;
             println!("{} files added", index.len());
         },
         Commands::Commit(CommitArgs { message }) => {
+            let repo = Repo::open(&config).expect("failed to open repository");
             let oid = repo.commit(&message)?;
             println!("made commit {oid}");
         },
         Commands::Status => {
+            let repo = Repo::open(&config).expect("failed to open repository");
             let summary = repo.status()?;
             println!("{summary:?}");
         },
         Commands::Branches => {
-            let branches = repo
-                .branches()?
-                .map(|(branch, branch_type)| {
-                    let branch_name = branch_name(&branch);
-                    format!("{branch_type:?} -- {branch_name}")
-                })
-                .join("\n");
-
-            println!("{branches}");
+            let repo = Repo::open(&config).expect("failed to open repository");
+            repo.branches()?.for_each(|(branch, branch_type)| {
+                let branch_name = branch_name(&branch);
+                println!("{branch_type:?} -- {branch_name}")
+            });
         },
         Commands::CurrentBranch => {
+            let repo = Repo::open(&config).expect("failed to open repository");
             let current_branch = repo.current_branch()?;
 
             let local = current_branch.local_name();
             let upstream = current_branch
                 .upstream_name()
                 .unwrap_or_else(|| "[No upstream branch tracked]".to_string());
+            
             println!("{local}:{upstream}");
         },
         Commands::Checkout(CheckoutArgs { branch_name }) => {
+            let repo = Repo::open(&config).expect("failed to open repository");
             let res = repo.checkout(&branch_name);
             println!("{res:?}");
         },
@@ -67,6 +76,7 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    Clone(CloneArgs),
     Add(AddArgs),
     Commit(CommitArgs),
     Status,
@@ -89,4 +99,9 @@ struct CommitArgs {
 #[derive(Args)]
 struct CheckoutArgs {
     branch_name: String,
+}
+
+#[derive(Args)]
+struct CloneArgs {
+    url: String,
 }
